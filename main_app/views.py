db1d017cadc8d9451cb493e4751498e6bd291801
laurenmarsh <laurenmdb@gmail.com>
2024-02-05 11:11:07 -0500

    urls

---
+++ main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Define the home view
+class Home(APIView):
+  def get(self, request):
+    content = {'message': 'Welcome to the rareplant_collector api home route!'}
+    return Response(content)
